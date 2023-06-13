07722a40b3de2f06783997517758541e32d5d059
charan14092003 <charankallempudi24@gmail.com>
2023-06-13 14:45:46 +0530

    linked pages home and search

--- components/HomeSection.jsx
+++ components/HomeSection.jsx
@@ -1,20 +1,25 @@
 import styles from "./HomeSection.module.css";
+import Link from "next/link";
 
 const HomeSection = () => {
   return (
     <div className={styles.home_section}>
-      <p className={styles.heading}>
-        <span className={styles.icons}>
-          <i className="bi bi-house-door"></i>
-        </span>
-        Home
-      </p>
-      <p className={styles.heading}>
-        <span className={styles.icons}>
-          <i className="bi bi-search"></i>
-        </span>
-        Search
-      </p>
+      <Link href="/">
+        <p className={styles.heading}>
+          <span className={styles.icons}>
+            <i className="bi bi-house-door"></i>
+          </span>
+          Home
+        </p>
+      </Link>
+      <Link href="/search">
+        <p className={styles.heading}>
+          <span className={styles.icons}>
+            <i className="bi bi-search"></i>
+          </span>
+          Search
+        </p>
+      </Link>
     </div>
   );
 };
